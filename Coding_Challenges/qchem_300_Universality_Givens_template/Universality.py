e0388a0ce166e437d4aad75ddc838f5c0c2e29f3
Rodrigo Chaves <86621071+rodchaves@users.noreply.github.com>
2022-04-05 17:43:32 -0300

    Add files via upload

---
+++ Coding_Challenges/qchem_300_Universality_Givens_template/Universality.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 21:45:41 2022
+
+@author: digao
+"""
+
+#! /usr/bin/python3
+
+import sys
+import numpy as np
+
+
+def givens_rotations(a, b, c, d):
+    """Calculates the angles needed for a Givens rotation to out put the state with amplitudes a,b,c and d
+    Args:
+        - a,b,c,d (float): real numbers which represent the amplitude of the relevant basis states (see problem statement). Assume they are normalized.
+    Returns:
+        - (list(float)): a list of real numbers ranging in the intervals provided in the challenge statement, which represent the angles in the Givens rotations,
+        in order, that must be applied.
+    """
+
+    # QHACK #
+    theta = 3*[0]
+    theta[2]= 2*np.arctan(-d/a)
+    theta[1]= 2*np.arctan(-c/b)
+    theta[0]= 2*np.arcsin(-b/np.cos(theta[1]/2))       
+ 
+    return theta
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    theta_1, theta_2, theta_3 = givens_rotations(
+        float(inputs[0]), float(inputs[1]), float(inputs[2]), float(inputs[3])
+    )
+    print(*[theta_1, theta_2, theta_3], sep=",")
